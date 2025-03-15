fb9da0638fe4eb61b533dff8967044cbb95beae9
princysami2609 <princyp6@gmail.com>
2025-03-15 16:38:32 +0530

    new project added

---
+++ src/test/java/com/cucumber/test/test1.java
@@ -0,0 +1,22 @@
+package com.cucumber.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void testCase1() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
+
+	}
+	
+
+}
